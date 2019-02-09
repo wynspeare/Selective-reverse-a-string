138f2ce25e50ee857a982e99a70360afe2869879
Wynspeare <wynspeare@gmail.com>
2019-02-09 16:39:22 -0600

    Python solution - needs refactoring

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Reverse a string
 
-Write an algorithm to reverse a string, without affecting any special characters, for example change `!!aPples_Ar3__Gr8!` to `!!8rG3rA_sel__pPa!`
+The goal is to write an algorithm to reverse a string, without affecting any special characters. For example - change `!!aPples_Ar3__Gr8!` to `!!8rG3rA_sel__pPa!`
+
 
 ```PYTHON
 def reverse_string(string):
--- reverse_string.py
+++ reverse_string.py
@@ -1,11 +1,46 @@
-# Weekend Algorithm
+import re
 
-# Write a function that reverses a string without affecting special caracters
-# “!!aPples_Ar3__Gr8!”
-# “!!8rG3rA_sel__pPa!
+def reverse_string(string):
+    word_array = list(string)
+    word_to_reverse = []
+    for index, char in enumerate(word_array):
+        if char.isalnum():
+            word_to_reverse.append(char)
+            word_array[index] = " "
+    for index, element in enumerate(word_array):
+        if element == " ":
+            word_array[index] = word_to_reverse[-1]
+            word_to_reverse.pop()
+    print(word_array)
+    print(''.join(word_array))
 
-# Rules
-# No built in methods
+reverse_string("!!aPples_Ar3__Gr8!")
 
-# Challenge
-# Do this in Ruby
+def reverse_string_regex(string):
+    word_array = list(string)
+    word_to_reverse = []
+    for index, char in enumerate(word_array):
+        if re.search(r'[a-zA-Z0-9]', char):
+            word_to_reverse.append(char)
+            word_array[index] = " "
+    for index, element in enumerate(word_array):
+        if element == " ":
+            word_array[index] = word_to_reverse[-1]
+            word_to_reverse.pop()
+    print(''.join(word_array))
+
+reverse_string_regex("!!aPples_Ar3__Gr8!")
+
+#Have a function that takes in a string
+    # Create WORD_ARRAY from string
+    # Create REVERSED_WORD empty array
+    #Loop thru WORD_ARRAY to access each CHAR
+        # If the CHAR is a LETTER or NUMBER
+            # Add to WORD_TO_REVERSE    "aPplesAr3Gr8"
+            # shift the CHAR onto beginning of REVERSED_WORD  "8rG3rAselpPa"
+            #Replace CHAR with " "      ["!", "!", " ", " ", " ", " ", ]
+    # Loop thru WORD_ARRAY 
+        # If ELEMENT is " "
+            # replace " " with WORD_TO_REVERSE's last element
+            # Remove last element of WORD_TO_REVERSE
+    # return joined WORD_ARRAY
